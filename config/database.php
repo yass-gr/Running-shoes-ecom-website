41f4517959100298de98870acaed6d68a95f890a
yassine grairi <grairiiyassine@gmail.com>
2026-06-25 20:00:59 +0100

    home controller fetches products and categories from model

--- config/database.php
+++ config/database.php
@@ -6,7 +6,7 @@ $username = "g13";
 $password = "yesnomaybeso";
 
 try{
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $pdo = new PDO("mysql:host=$hostname;dbname=$dbname", $username, $password);
 
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
--- controllers/HomeController.php
+++ controllers/HomeController.php
@@ -4,6 +4,24 @@ class HomeController
 {
     public function index(): void
     {
+        require_once __DIR__ . "/../config/database.php";
+        require_once __DIR__ . "/../models/product.php";
+        require_once __DIR__ . "/../models/category.php";
+        require_once __DIR__ . "/../models/product_img.php";
+
+        $productModel = new Product($pdo);
+        $categoryModel = new Category($pdo);
+        $imgModel = new ProductImg($pdo);
+
+        $newArrivals = $productModel->getTopSellers(8);
+        $categories = $categoryModel->findAll();
+
+        $newArrivalsImgs = [];
+        foreach ($newArrivals as $p) {
+            $img = $imgModel->findByVariantProduct($p["id"]);
+            $newArrivalsImgs[$p["id"]] = $img[0]["url"] ?? null;
+        }
+
         require __DIR__ . "/../views/home.php";
     }
 }
