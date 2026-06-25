5262c77fd380cac858fd902e7fd965013e759ad3
yassine grairi <grairiiyassine@gmail.com>
2026-06-25 20:07:52 +0100

    disable emulated prepares so LIMIT params are sent as ints

--- config/database.php
+++ config/database.php
@@ -10,6 +10,7 @@ try{
     $pdo = new PDO("mysql:host=$hostname;dbname=$dbname", $username, $password);
 
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 
 }catch(PDOException $e){
     echo "erreur : " . $e->getMessage();
