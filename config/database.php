bf3167b73f44bb84aad9ce0fcf4d65a4fe1bc9fd
yassine grairi <grairiiyassine@gmail.com>
2026-06-25 20:02:39 +0100

    env file for db config

--- config/database.php
+++ config/database.php
@@ -1,9 +1,10 @@
 <?php
 
-$hostname = "localhost";
-$dbname = "runningdb";
-$username = "g13";
-$password = "yesnomaybeso";
+$env = parse_ini_file(__DIR__ . "/../.env");
+$hostname = $env["DB_HOST"] ?? "localhost";
+$dbname = $env["DB_NAME"] ?? "runningdb";
+$username = $env["DB_USER"] ?? "g13";
+$password = $env["DB_PASS"] ?? "yesnomaybeso";
 
 try{
     $pdo = new PDO("mysql:host=$hostname;dbname=$dbname", $username, $password);
