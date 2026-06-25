8872b47cd769d5567b3389f648e51c353dfb0190
yassine grairi <grairiiyassine@gmail.com>
2026-06-26 00:15:21 +0100

    update routing for new controllers

--- routes.php
+++ routes.php
@@ -3,10 +3,15 @@
 require_once __DIR__ . "/controllers/HomeController.php";
 require_once __DIR__ . "/controllers/ProductController.php";
 require_once __DIR__ . "/controllers/ShopAllController.php";
+require_once __DIR__ . "/controllers/MensController.php";
+require_once __DIR__ . "/controllers/WomensController.php";
+require_once __DIR__ . "/controllers/SaleController.php";
+require_once __DIR__ . "/controllers/NewArrivalsController.php";
 require_once __DIR__ . "/controllers/CartController.php";
 require_once __DIR__ . "/controllers/OrderController.php";
 require_once __DIR__ . "/controllers/AuthController.php";
 require_once __DIR__ . "/controllers/AdminController.php";
+require_once __DIR__ . "/controllers/DeliveryController.php";
 
 $route = $_GET["route"] ?? "home";
 
@@ -22,9 +27,25 @@ switch ($route) {
         break;
 
     case "mens":
+        $controller = new MensController();
+        $controller->index();
+        break;
+
     case "womens":
+        $controller = new WomensController();
+        $controller->index();
+        break;
+
     case "sale":
+        $controller = new SaleController();
+        $controller->index();
+        break;
+
     case "new-arrivals":
+        $controller = new NewArrivalsController();
+        $controller->index();
+        break;
+
     case "product":
         $controller = new ProductController();
         $controller->index();
@@ -50,6 +71,11 @@ switch ($route) {
 
     case "admin":
         $controller = new AdminController();
+        $controller->index();
+        break;
+
+    case "delivery":
+        $controller = new DeliveryController();
         $controller->index();
         break;
 
